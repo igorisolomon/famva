ac542486b6011db458cf5b11c831c57a37b0f444
0xigori <solomon.o.igori@gmail.com>
2026-04-18 19:46:56 +0100

    added Meta Pixel script

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Analytics } from '@vercel/analytics/next'
 import type { Metadata } from 'next'
 import { Inter, Poppins } from 'next/font/google'
 import { headers } from 'next/headers'
+import Script from 'next/script'
 
 import './globals.css'
 
@@ -96,6 +97,22 @@ export default function RootLayout({
       <body className="font-sans antialiased" suppressHydrationWarning>
         {children}
         <Analytics />
+        <Script id="meta-pixel" strategy="afterInteractive">{`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '860619920391247');
+          fbq('track', 'PageView');
+        `}</Script>
+        <noscript><img height="1" width="1" className="hidden"
+          src="https://www.facebook.com/tr?id=860619920391247&ev=PageView&noscript=1"
+          alt=""
+        /></noscript>
       </body>
     </html>
   )
